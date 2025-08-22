01647c2284f0200ee276f3830a55eba1b0950abd
Khanh N <khanh.nguyen120@education.nsw.gov.au>
2025-08-22 12:58:50 +1000

    finish

--- project/py_scripts/colorsensor.py
+++ project/py_scripts/colorsensor.py
@@ -1,19 +1,18 @@
 from PiicoDev_VEML6040 import PiicoDev_VEML6040
 
-colorSensor = PiicoDev_VEML6040()
-
 class Colour_Sensor:
     def __init__(self, debug):
         self.__debug = debug
+        self.colorSensor = PiicoDev_VEML6040()
 
     def check_colour(self):
         if self.__debug:
-            print("Checking Coulur")
+            print("Checking Colour")
 
-        data = colorSensor.readHSV()
+        data = self.colorSensor.readHSV()
         hue = data['hue']
 
-        if hue > 75 and hue < 85:
+        if 75 < hue < 85:
             return "green"
         else:
             return "not green"
